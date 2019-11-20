29053dd33d46d994120c05f80f3295b3b917baed
Moser <nmoser@ah-4v0spc2.equian.com>
2019-11-20 16:16:12 -0600

    icon and dropdown

--- src/components/common/appbar/Appbar.js
+++ src/components/common/appbar/Appbar.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import {AppBar, Toolbar, Typography, Button, IconButton, makeStyles} from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu'
 import Login from '../login/Login';
+import Dropdown from '../dropdown/Dropdown';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -18,14 +19,16 @@ const useStyles = makeStyles(theme => ({
 function Appbar() {
   const classes = useStyles()
   const [openDialog, setOpenDialog] = useState(false)
+  const [anchorEl, setAnchorEl] = useState(null)
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar color="primary">
-          <IconButton edge="start" className={classes.menuButton} color="secondary" aria-label="menu">
+          <IconButton edge="start" className={classes.menuButton} color="secondary" aria-label="menu" onClick={e => setAnchorEl(e.currentTarget)}> 
             <MenuIcon />
           </IconButton>
+          <Dropdown open={!!anchorEl} anchorEl={anchorEl} close={() => setAnchorEl(null)}/>
           <Typography variant="h6" className={classes.title}>
             Moser Family
           </Typography>
--- src/components/common/dropdown/Dropdown.js
+++ src/components/common/dropdown/Dropdown.js
@@ -1,2 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { MenuItem, Menu } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
 
+const useStyles = makeStyles(theme => ({
+    menu: {
+        marginTop: 50
+    }
+}))
+
+const options = [
+    'Videos',
+    'Quotes',
+    'Pictures'
+  ];
+
+function Dropdown({open, anchorEl, close}) {
+    const classes = useStyles()
+    const [selectedIndex, setSelectedIndex] = useState(0)
+
+    const createMenuItems = (item, key) => {
+        return (
+            <MenuItem
+              key={key}
+              selected={key === selectedIndex}
+              onClick={event => onClick(event, key)}>
+                  {item}
+              </MenuItem>
+        )
+    }
+
+    const onClick = (event, index) => {
+        setSelectedIndex(index)
+        close()
+    }
+    return (
+        <Menu open={open} className={classes.menu} anchorEl={anchorEl}>
+            {options.map(createMenuItems)}
+        </Menu>
+    )
+}
+
+export default Dropdown
